2c28fb6b846067769c53b5aaf845b38c38c0c788
Karanami <piotrlesicki04@gmail.com>
2025-04-11 22:46:04 +0200

    errors work

--- firmware/Components/ErrorChecker/Inc/error_checker.hpp
+++ firmware/Components/ErrorChecker/Inc/error_checker.hpp
@@ -14,29 +14,37 @@ struct Error
      *          it is advised to use unique error codes if one error type is checked 
      *          against multipe unique conditions
      *  @return The pointed function should return 0 when no errors where found, a value 
-     *          bigger than 0 when an error was found
+     *          bigger than 0 when an error was found. This value is used to identify the
+     *          error sub type. It is advised to use unique error codes for each error sub 
+     *          type.
      */
     uint32_t (*condition)(void) { nullptr };
-    
+    /**
+     *  @brief  Pointer to a function which should parse the error code and return a 
+     *          string with the error message.
+     *  @note   This function is optional, it should be used to parse the error code and 
+     *          return a string with the error message. It is advised to use unique error 
+     *          codes for each error sub type.
+     *  @param  code Error code to be parsed
+     *  @return String with the error message, it should be a static string.
+     */
+    const char* (*parse)(uint32_t) { nullptr };
     /* PRIVATE */
     Error *next_error { nullptr };
-    uint32_t id { std::numeric_limits<uint32_t>::max() };
 };
 
-template<size_t SIZE>
-class ErrorLogger
-{
-public:
-
-private:
-};
-
+/**
+ *  @brief  Error checker class which is used to check for errors
+ *  @note   This class is used to check for errors in the system and log them if they 
+ *          are found. It uses a linked list to store the errors and a variadic template 
+ *          function to add new errors to the list.
+ */
 class ErrorChecker
 {
 private:
     /**
      *  @brief  Add edges helper function
-     *  @param  error Pointer to the next edge to be initialized by the state machine
+     *  @param  error Pointer to the next edge to be initialized by the error checker
      */
     void add_errors_helper(Error *error);
 public:
@@ -52,10 +60,16 @@ public:
         (add_erros_helper(&errors), ...);
     }
 
+    /**
+     *  @brief  Function which checks for errors in the system
+     *  @note   This function should be called periodically to check for errors in the 
+     *          system. It will check all the errors in the list and log them if they 
+     *          are found.
+     */
+    void check_errors(void);
 private:
     /* Added errors behave like list Error checker remembers the first one */
     Error *next_error { nullptr };
     /* Last added error */
     Error *last_error { nullptr };
-    uint32_t error_types_count { };
 };
---
+++ firmware/Components/ErrorChecker/Inc/error_logger.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "main.h"
+
+struct ErrorLog
+{
+    /* Error name */
+    const char *name { "" };
+    /* Error code */
+    uint32_t code { 0 };
+    /* Error message */
+    const char *message { "" };
+}
+
+template<size_t SIZE>
+class ErrorLogger
+{
+public:
+    /**
+     *  @brief  Function which logs an error
+     *  @param  name Name of the error
+     *  @param  code Error code
+     *  @param  message Error message
+     *  @note   This function will log the error in the error logger buffer. If the buffer
+     *          is full, the error will not be logged and the overflow flag will be set to
+     *          true.
+     */    
+    void log_error(const char *name, uint32_t code, const char *message)
+    {
+        if (index < size)
+        {
+            buffer[index].name = name;
+            buffer[index].code = code;
+            buffer[index].message = message;
+            index++;
+        }
+        else
+        {
+            overflow = true;
+        }
+    }
+private:
+    /* Error logger buffer */
+    ErrorLog buffer[SIZE] { 0 };
+    /* Error logger buffer index */
+    size_t index { 0 };
+    /* Error logger buffer size */
+    size_t size { SIZE };
+    /* Error logger buffer overflow flag */
+    bool overflow { false };
+};
